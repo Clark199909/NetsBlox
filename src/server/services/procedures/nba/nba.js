0316ed47849ed619ecbf229e997077bab07a6d5a
Zhengkai Xie <zhengkai.xie@vanderbilt.edu>
2020-03-05 23:43:12 -0500

    3/5

--- src/server/services/procedures/nba/nba.js
+++ src/server/services/procedures/nba/nba.js
@@ -1,6 +1,7 @@
 
 const ApiConsumer = require('../utils/api-consumer');
 const nba = new ApiConsumer('nba', 'https://www.balldontlie.io/api/v1/',{cache: {ttl: 24*60*60}});
+const nbaImage = new ApiConsumer('nbaImage', 'https://nba-players.herokuapp.com/', {cache: {ttl: 24*60*60}});
 
 const nameError = err => {
     if(err.statusCode === 404) {
@@ -20,24 +21,22 @@ const idError = err => {
     }
 }
 
-
-nba.playerId = function(name) {
-    name = name.target.value.split(" ").join("_");
+nba.teamInfo = function(id, field) {
     field = '.' + field;
     const options = {
-        path: `players?search=${name}`,
+        path: `teams/${id}`,
     };
-    return this._sendAnswer(options, '.id')
+    return this._sendAnswer(options, field)
         .catch(err => {
-            if (nameError(err)) {
-                return this.response.status(500).send(nameError(err));
+            if (idError(err)) {
+                return this.response.status(500).send(idError(err));
             }
             throw err;
         });
 }
 
 nba.playerInfo = function(name, field) {
-    name = name.target.value.split(" ").join("_");
+    name = name.split(" ").join("_");
     field = '.' + field;
     const options = {
         path: `players?search=${name}`,
@@ -53,27 +52,67 @@ nba.playerInfo = function(name, field) {
         });
 }
 
-nba.teamInfo = function(id, field) {
+
+nba.playerstats = function(id, season, field) {
     field = '.' + field;
     const options = {
-        path: `teams/${id}`,
+        path: `season_averages?season=${season}&player_ids[]=${id}`,
     };
-    return this._sendAnswer(options, '.id')
+    return this._sendAnswer(options, field)
         .catch(err => {
-            if (idError(err)) {
+            if(idError(err)) {
                 return this.response.status(500).send(idError(err));
-            }
+            } else if(fieldError(err)) {
+                return this.response.status(500).send(fieldError(err));
+            } 
             throw err;
         });
 }
 
-nba.playerFirstName = function(name) {return this.playerInfo(name, 'first_name');}
-nba.playerLastName = function(name) {return this.playerInfo.call(name, 'last_name');}
-nba.playerPosition = function(name) {return this.playerInfo.call(name, 'position');}
-nba.playerHeightFeet = function(name) {return this.playerInfo.call(name, 'height_feet');}
-nba.playerHeightInches = function(name) {return this.playerInfo.call(name, 'height_inches');}
-nba.playerWeightPounds = function(name) {return this.playerInfo.call(name, 'weight_pounds');}
-//nba.playerTeam = function(name){return playerInfo.call(this, name, 'team', 'full_name')}
+nbaImage.playerImage = function(first_name, last_name) {
+    const options = {
+        path: `players/${last_name}/${first_name}`,
+    };
+    return this._sendImage(options)
+        .catch(err => {
+            if(nameError(err)) {
+                return this.response.status(500).send(nameError(err));
+            } 
+            throw err;
+        });
+}
+
+nba.playerId = function(name) {return this.playerInfo(name, 'data[0][id]');}
+nba.playerFirstName = function(name) {return this.playerInfo(name, 'data[0][first_name]');}
+nba.playerLastName = function(name) {return this.playerInfo(name, 'data[0][last_name]');}
+nba.playerPosition = function(name) {return this.playerInfo(name, 'data[0][position]');}
+nba.playerHeightFeet = function(name) {return this.playerInfo(name, 'data[0][height_feet]');}
+nba.playerHeightInches = function(name) {return this.playerInfo(name, 'data[0][height_inches]');}
+nba.playerWeightPounds = function(name) {return this.playerInfo(name, 'data[0][weight_pounds]');}
+
+nba.playerAveragePoints = function(id, season) {return this.playerstats(id, season, 'data[0][pts]')}
+nba.playerAverageMinutes = function(id, season) {return this.playerstats(id, season, 'data[0][min]')}
+nba.playerAverageRebounds = function(id, season) {return this.playerstats(id, season, 'data[0][reb]')}
+nba.playerAverageAssits = function(id, season) {return this.playerstats(id, season, 'data[0][ast]')}
+nba.playerAverageBlocks = function(id, season) {return this.playerstats(id, season, 'data[0][blk]')}
+nba.playerAverageSteals = function(id, season) {return this.playerstats(id, season, 'data[0][stl]')}
+nba.playerAverageOffensiveRebounds = function(id, season) {return this.playerstats(id, season, 'data[0][oreb]')}
+nba.playerAverageDefensiveRebounds = function(id, season) {return this.playerstats(id, season, 'data[0][dreb]')}
+nba.playerAverageTurnOvers = function(id, season) {return this.playerstats(id, season, 'data[0][turnover]')}
+nba.playerAverageFouls = function(id, season) {return this.playerstats(id, season, 'data[0][pf]')}
+nba.playerAverageFieldGoalAttempts = function(id, season) {return this.playerstats(id, season, 'data[0][fga]')}
+nba.playerAverageFieldGoalMade = function(id, season) {return this.playerstats(id, season, 'data[0][fgm]')}
+nba.playerAverageFieldGoalPercent = function(id, season) {return this.playerstats(id, season, 'data[0][fg_pct]')}
+nba.playerAverageThreePointsAttempts = function(id, season) {return this.playerstats(id, season, 'data[0][fg3a]')}
+nba.playerAverageThreePointsMade = function(id, season) {return this.playerstats(id, season, 'data[0][fg3m]')}
+nba.playerAverageThreePointsPercent = function(id, season) {return this.playerstats(id, season, 'data[0][fg3_pct]')}
+nba.playerAverageFreeThrowAttempts = function(id, season) {return this.playerstats(id, season, 'data[0][fta]')}
+nba.playerAverageFreeThrowMade = function(id, season) {return this.playerstats(id, season, 'data[0][ftm]')}
+nba.playerAverageFreeThrowPercent = function(id, season) {return this.playerstats(id, season, 'data[0][ft_pct]')}
+
+
+nba.playerHeadShot = function(first_name, last_name) {return nbaImage.playerImage(first_name, last_name)}
+
 
 nba.teamAbbreviation = function(id) {return this.teamInfo.call(id, 'abbreviation');}
 nba.teamCity = function(id) {return this.teamInfo.call(id, 'city');}
