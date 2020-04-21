abb965d3d8d4ae8aa4eaaccd50e8a74dadd288c8
Zhengkai Xie <zhengkai.xie@vanderbilt.edu>
2020-04-20 22:23:29 -0400

    4/20

--- src/server/services/procedures/nba/nba.js
+++ src/server/services/procedures/nba/nba.js
@@ -36,7 +36,7 @@ nba.teamInfo = function(id, field) {
 }
 
 nba.playerInfo = function(name, field) {
-    name = name.split(" ").join("_");
+    name = ('' + name).split(" ").join("_");
     field = '.' + field;
     const options = {
         path: `players?search=${name}`,
@@ -85,33 +85,134 @@ nbaImage.playerImage = function(first_name, last_name) {
 nba.playerId = function(name) {return this.playerInfo(name, 'data[0][id]');}
 nba.playerFirstName = function(name) {return this.playerInfo(name, 'data[0][first_name]');}
 nba.playerLastName = function(name) {return this.playerInfo(name, 'data[0][last_name]');}
-nba.playerPosition = function(name) {return this.playerInfo(name, 'data[0][position]');}
+nba.playerPosition = async function(name) {
+    let positionInfo = await this.playerInfo(name, 'data[0][position]');
+    if(positionInfo === 'G') {
+        return 'Guard';
+    } else if(positionInfo === 'F') {
+        return 'Forward';
+    } else if(positionInfo === 'C') {
+        return 'Center';
+    } else if(positionInfo === 'F-C') {
+        return 'Forward or Center';
+    } else if(positionInfo === 'G-F') {
+        return 'Guard or Forward';
+    }
+}
 nba.playerHeightFeet = function(name) {return this.playerInfo(name, 'data[0][height_feet]');}
 nba.playerHeightInches = function(name) {return this.playerInfo(name, 'data[0][height_inches]');}
 nba.playerWeightPounds = function(name) {return this.playerInfo(name, 'data[0][weight_pounds]');}
+nba.playerteamName = function(name) {return this.playerInfo(name, 'data[0][team][name]');}
+nba.playerteamCity = function(name) {return this.playerInfo(name, 'data[0][team][city]');}
 
-nba.playerAveragePoints = function(id, season) {return this.playerstats(id, season, 'data[0][pts]')}
-nba.playerAverageMinutes = function(id, season) {return this.playerstats(id, season, 'data[0][min]')}
-nba.playerAverageRebounds = function(id, season) {return this.playerstats(id, season, 'data[0][reb]')}
-nba.playerAverageAssits = function(id, season) {return this.playerstats(id, season, 'data[0][ast]')}
-nba.playerAverageBlocks = function(id, season) {return this.playerstats(id, season, 'data[0][blk]')}
-nba.playerAverageSteals = function(id, season) {return this.playerstats(id, season, 'data[0][stl]')}
-nba.playerAverageOffensiveRebounds = function(id, season) {return this.playerstats(id, season, 'data[0][oreb]')}
-nba.playerAverageDefensiveRebounds = function(id, season) {return this.playerstats(id, season, 'data[0][dreb]')}
-nba.playerAverageTurnOvers = function(id, season) {return this.playerstats(id, season, 'data[0][turnover]')}
-nba.playerAverageFouls = function(id, season) {return this.playerstats(id, season, 'data[0][pf]')}
-nba.playerAverageFieldGoalAttempts = function(id, season) {return this.playerstats(id, season, 'data[0][fga]')}
-nba.playerAverageFieldGoalMade = function(id, season) {return this.playerstats(id, season, 'data[0][fgm]')}
-nba.playerAverageFieldGoalPercent = function(id, season) {return this.playerstats(id, season, 'data[0][fg_pct]')}
-nba.playerAverageThreePointsAttempts = function(id, season) {return this.playerstats(id, season, 'data[0][fg3a]')}
-nba.playerAverageThreePointsMade = function(id, season) {return this.playerstats(id, season, 'data[0][fg3m]')}
-nba.playerAverageThreePointsPercent = function(id, season) {return this.playerstats(id, season, 'data[0][fg3_pct]')}
-nba.playerAverageFreeThrowAttempts = function(id, season) {return this.playerstats(id, season, 'data[0][fta]')}
-nba.playerAverageFreeThrowMade = function(id, season) {return this.playerstats(id, season, 'data[0][ftm]')}
-nba.playerAverageFreeThrowPercent = function(id, season) {return this.playerstats(id, season, 'data[0][ft_pct]')}
+nba.playerAveragePoints = async function(name, season) {
+    let id = await this.playerId(name);
+    return this.playerstats(id, season, 'data[0][pts]');
+}
+nba.playerAverageMinutes = async function(name, season) {
+    let id = await this.playerId(name);
+    return this.playerstats(id, season, 'data[0][min]');
+}
+nba.playerAverageRebounds = async function(name, season) {
+    let id = await this.playerId(name);
+    return this.playerstats(id, season, 'data[0][reb]');
+}
+nba.playerAverageAssits = async function(name, season) {
+    let id = await this.playerId(name);
+    return this.playerstats(id, season, 'data[0][ast]');
+}
+nba.playerAverageBlocks = async function(name, season) {
+    let id = await this.playerId(name);
+    return this.playerstats(id, season, 'data[0][blk]');
+}
+nba.playerAverageSteals = async function(name, season) {
+    let id = await this.playerId(name);
+    return this.playerstats(id, season, 'data[0][stl]');
+}
+nba.playerAverageOffensiveRebounds = async function(name, season) {
+    let id = await this.playerId(name);
+    return this.playerstats(id, season, 'data[0][oreb]');
+}
+nba.playerAverageDefensiveRebounds = async function(name, season) {
+    let id = await this.playerId(name);
+    return this.playerstats(id, season, 'data[0][dreb]');
+}
+nba.playerAverageTurnOvers = async function(name, season) {
+    let id = await this.playerId(name);
+    return this.playerstats(id, season, 'data[0][turnover]');
+}
+nba.playerAverageFouls = async function(name, season) {
+    let id = await this.playerId(name);
+    return this.playerstats(id, season, 'data[0][pf]');
+}
+nba.playerAverageFieldGoalAttempts = async function(name, season) {
+    let id = await this.playerId(name);
+    return this.playerstats(id, season, 'data[0][fga]');
+}
+nba.playerAverageFieldGoalMade = async function(name, season) {
+    let id = await this.playerId(name);
+    return this.playerstats(id, season, 'data[0][fgm]');
+}
+nba.playerAverageFieldGoalPercent = async function(name, season) {
+    let id = await this.playerId(name);
+    let percentInDecimal = await this.playerstats(id, season, 'data[0][fg_pct]');
+    return Math.floor(percentInDecimal * 100) + '%';
+}
+nba.playerAverageThreePointsAttempts = async function(name, season) {
+    let id = await this.playerId(name);
+    return this.playerstats(id, season, 'data[0][fg3a]');
+}
+nba.playerAverageThreePointsMade = async function(name, season) {
+    let id = await this.playerId(name);
+    return this.playerstats(id, season, 'data[0][fg3m]');
+}
+nba.playerAverageThreePointsPercent = async function(name, season) {
+    let id = await this.playerId(name);
+    let percentInDecimal = await this.playerstats(id, season, 'data[0][fg3_pct]');
+    return Math.floor(percentInDecimal * 100) + '%';
+}
+nba.playerAverageFreeThrowAttempts = async function(name, season) {
+    let id = await this.playerId(name);
+    return this.playerstats(id, season, 'data[0][fta]');
+}
+nba.playerAverageFreeThrowMade = async function(name, season) {
+    let id = await this.playerId(name);
+    return this.playerstats(id, season, 'data[0][ftm]');
+}
+nba.playerAverageFreeThrowPercent = async function(name, season) {
+    let id = await this.playerId(name);
+    let percentInDecimal = await this.playerstats(id, season, 'data[0][ft_pct]');
+    return Math.floor(percentInDecimal * 100) + '%';
+}
 
+nba.playerHeadShot = async function(name) {
+    nbaImage.response = this.response;
+    let first_name = await this.playerFirstName(name);
+    let last_name = await this.playerLastName(name);
+    return nbaImage.playerImage(first_name, last_name);
+}
 
-nba.playerHeadShot = function(first_name, last_name) {return nbaImage.playerImage(first_name, last_name)}
+nba.NBAEfficiency = async function(name, season) {
+    let pts = await this.playerAveragePoints(name, season);
+    let ast = await this.playerAverageAssits(name, season);
+    let reb = await this.playerAverageRebounds(name, season);
+    let blk = await this.playerAverageBlocks(name, season);
+    let stl = await this.playerAverageSteals(name, season);
+    let tov = await this.playerAverageTurnOvers(name, season);
+    let ga = await this.playerAverageFieldGoalAttempts(name, season);
+    let gm = await this.playerAverageFieldGoalMade(name, season);
+    let fa = await this.playerAverageFreeThrowAttempts(name, season);
+    let fm = await this.playerAverageFreeThrowMade(name, season);
+    return (pts + ast + reb + blk + stl - tov - (ga - gm) - (fa - fm));
+}
+
+nba.playerAssistOverTurnovers = async function(name, season) {
+    let ast = await this.playerAverageAssits(name, season);
+    let tov = await this.playerAverageTurnOvers(name, season);
+    return (ast/tov);
+}
+
+var teamDict = new Object();
 
 
 nba.teamAbbreviation = function(id) {return this.teamInfo.call(id, 'abbreviation');}
